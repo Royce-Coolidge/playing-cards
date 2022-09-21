45f7a1fbf19f5f54ca18c9fec938259ad96e1400
Rowley Thompson <rjw@outlookenergy.co.uk>
2022-09-21 13:48:42 +0100

    display sort and suffle funstions implemented

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,101 +1,109 @@
-import "./styles.css"
+import "./styles.css";
 import { useEffect, useState } from "react";
 
-const suits = ["heart", "club", "diamond", "spade"]
+const suits = ["heart", "club", "diamond", "spade"];
 const Cards = () => {
+  const [cards, setCards] = useState([]);
+  const [removed, setRemoved] = useState([]);
 
-    const [cards, setCards] = useState([]);
-    const [removed, setRemoved] = useState([]);
-
-    useEffect(() => {
-      const createPackOfCards = () => {
-        let i = 1;
-        let suit_index = 0;
-        let suit_card_value = 1;
-
-        let cards = [];
-        while (i <= 52) {
-          if (suit_card_value % 14 === 0) {
-            suit_card_value = 1;
-            suit_index++;
-          }
-          cards.push({ suit_index: suit_index, value: suit_card_value, id: i });
-          suit_card_value++;
-          i++;
-        }
-        return cards;
-      };
-
-      const cards = createPackOfCards();
-      setCards(cards);
-    }, []);
-
-    const shuffle = () => {
-      let shuffled = [];
-      let usedCardIndexes = [];
-
+  useEffect(() => {
+    const createPackOfCards = () => {
       let i = 1;
-      while (i <= cards.length) {
-        // Math.random returns floating point number between 0 - 1
-        // multiplying  0 by array.length will give, and 0.99 will give 52
-        // becasue array is not zero based. wEcan just round the numbder rather than floor it
-        let randomNumber = Math.floor(Math.random() * cards.length);
-        if (!usedCardIndexes.includes(randomNumber)) {
-          shuffled.push(cards[randomNumber]);
-          usedCardIndexes.push(randomNumber);
-          i++;
+      let suit_index = 0;
+      let suit_card_value = 1;
+
+      let cards = [];
+      while (i <= 52) {
+        if (suit_card_value % 14 === 0) {
+          suit_card_value = 1;
+          suit_index++;
         }
+        cards.push({ suit_index: suit_index, value: suit_card_value, id: i });
+        suit_card_value++;
+        i++;
       }
-      setCards(shuffled);
+      return cards;
     };
 
-    const sortCards = () => {
-      setCards((cards) =>
-        cards.sort((a, b) => {
-          a.value - b.value;
-        })
-      );
-    };
+    const cards = createPackOfCards();
+    setCards(cards);
+  }, []);
 
-    function displayCards(card) {
-        let value = card.value
-        // if (value === 1) value = "A"
-        // if (value === 11) value = "J"
-        // if (value === 12) value = "Q"
-        // if (value === 13) value = "K"
-      
-        let pips = []
-        let valueAsNumder = parseInt(card.value)
-        
-        if (isNaN(value)) {
-          console.log(value)
-        } else {
-            pips = new Array(valueAsNumder)
-            
-        }
+  const shuffle = () => {
+    let shuffled = [];
+    let usedCardIndexes = [];
 
-        console.log(pips.map((pip) => <div className="pip"></div>));
-        return (
-          <div key={card.id} className="card" value={"10"} suit={suits[card.suit_index]}>
-                {pips.map(pip => <div className="pip"></div>)}
-                <div className="corner-number top">{value}</div>
-                <div className="corner-number bottom">{value}</div>
-          </div>
-        );
+    let i = 1;
+    while (i <= cards.length) {
+      // Math.random returns floating point number between 0 - 1
+      // multiplying  0 by array.length will give, and 0.99 will give 52
+      // becasue array is not zero based. wEcan just round the numbder rather than floor it
+      let randomNumber = Math.floor(Math.random() * cards.length);
+      if (!usedCardIndexes.includes(randomNumber)) {
+        shuffled.push(cards[randomNumber]);
+        usedCardIndexes.push(randomNumber);
+        i++;
+      }
     }
+    setCards(shuffled);
+  };
 
-    console.log()
-    
+  const sortCards = () => {
+    let sorted = [...cards].sort((a, b) => {
+      return a.value - b.value;
+    });
+    setCards(sorted);
+  };
 
-    
+  function displayCards(card) {
+    let pips = [];
+      let value 
+      switch (card.value) {
+          case 1: value = "A"
+              break;
+          case 11: value = "J"
+              break;
+          case 12: value = "Q"
+              break;
+          case 13: value = "K"
+              break;
+          default:
+              value = card.value
+      }
+      let valueAsNumber = parseInt(value)
+      if (isNaN(valueAsNumber)) {
+          pips = new Array(1).fill(1);
+      }
 
+    pips = new Array(value).fill(value);
 
     return (
-        <section className="body">
-            {displayCards({ suit_index: 0, value: 10, id: 4 })}
-        
-      </section>
+      <div
+        key={card.id}
+        className="card"
+        value={value}
+        suit={suits[card.suit_index]}
+      >
+        {pips.map((pip) => (
+          <div className="pip"></div>
+        ))}
+        <div className="corner-number top">{value}</div>
+        <div className="corner-number bottom">{value}</div>
+      </div>
     );
-}
+  }
+
+  return (
+    <>
+      <section className="board">
+        {cards.map((card) => displayCards(card))}
+      </section>
+      <section className="dealer">
+        <button onClick={shuffle}>Shuffle</button>
+        <button onClick={sortCards}>Sort</button>
+      </section>
+    </>
+  );
+};
 
 export default Cards;
